c300118e5be7ea2f4616709d6f39526d67ef734d
baptistejouin <baptiste.jouin44@gmail.com>
2023-02-17 18:33:13 +0100

    feat: exo6 partie listes chainées, wip DynaTableau

--- TP1/exo6.cpp
+++ TP1/exo6.cpp
@@ -2,117 +2,239 @@
 
 using namespace std;
 
-struct Noeud{
+struct Noeud
+{
     int donnee;
-    Noeud* suivant;
+    Noeud *suivant;
 };
 
-struct Liste{
-    Noeud* premier;
-    // your code
-};
-
-struct DynaTableau{
-    int* donnees;
-    // your code
-};
-
-
-void initialise(Liste* liste)
+struct Liste
 {
+    Noeud *premier;
+};
 
+struct DynaTableau
+{
+    int *donnees, size, capacity;
+};
+
+void initialise(Liste *liste)
+{
+    liste->premier = nullptr;
 }
 
-bool est_vide(const Liste* liste)
+bool est_vide(const Liste *liste)
 {
+    if (liste->premier == nullptr)
+    {
+        return true;
+    }
     return false;
 }
 
-void ajoute(Liste* liste, int valeur)
+void ajoute(Liste *liste, int valeur)
 {
+    Noeud *currentNode = liste->premier;
+    Noeud *newNode = new Noeud();
 
+    if (!newNode)
+    {
+        std::cout << "Allocation failed" << std::endl;
+        return;
+    }
+
+    newNode->donnee = valeur;
+
+    if (est_vide(liste))
+    {
+        liste->premier = newNode;
+        return;
+    }
+
+    while (currentNode->suivant != nullptr)
+    {
+        currentNode = currentNode->suivant;
+    }
+
+    currentNode->suivant = newNode;
 }
 
-void affiche(const Liste* liste)
+void affiche(const Liste *liste)
 {
+    Noeud *currentNode = liste->premier;
 
+    if (est_vide(liste))
+    {
+        std::cout << "Empty list !" << std::endl;
+        return;
+    }
+    while (currentNode->suivant != nullptr)
+    {
+        std::cout << currentNode->donnee << ", " << std::endl;
+        currentNode = currentNode->suivant;
+    }
 }
 
-int recupere(const Liste* liste, int n)
+// Retourne le nième entier de la structure
+int recupere(const Liste *liste, int n)
+{
+    Noeud *currentNode = liste->premier;
+
+    for (size_t i = 0; i <= n; i++)
+    {
+        currentNode = currentNode->suivant;
+    }
+
+    return currentNode->donnee;
+}
+
+// Retourne l’index de valeur dans la structure ou -1 s’il n’existe pas
+int cherche(const Liste *liste, int valeur)
+{
+    Noeud *currentNode = liste->premier;
+    int index = 0;
+
+    while (currentNode->donnee != valeur && currentNode->suivant != nullptr)
+    {
+        index++;
+        currentNode = currentNode->suivant;
+    }
+
+    return currentNode->suivant == nullptr ? -1 : index;
+}
+
+// Dedéfini la nième valeur de la structure avec valeur
+void stocke(Liste *liste, int n, int valeur)
+{
+    Noeud *currentNode = liste->premier;
+
+    for (size_t i = 0; i <= n; i++)
+    {
+        currentNode = currentNode->suivant;
+    }
+
+    currentNode->donnee = valeur;
+}
+
+// TODO:
+void ajoute(DynaTableau *tableau, int valeur)
+{
+}
+
+// TODO:
+void initialise(DynaTableau *tableau, int capacite)
+{
+    tableau->donnees = nullptr;
+}
+
+// TODO:
+bool est_vide(const DynaTableau *liste)
+{
+    if (liste->donnees == nullptr)
+    {
+        return true;
+    }
+    return false;
+}
+
+// TODO:
+void affiche(const DynaTableau *tableau)
+{
+}
+
+// TODO:
+int recupere(const DynaTableau *tableau, int n)
 {
     return 0;
 }
 
-int cherche(const Liste* liste, int valeur)
+// TODO:
+int cherche(const DynaTableau *tableau, int valeur)
 {
     return -1;
 }
 
-void stocke(Liste* liste, int n, int valeur)
+// TODO:
+void stocke(DynaTableau *tableau, int n, int valeur)
 {
-
 }
 
-void ajoute(DynaTableau* tableau, int valeur)
+// Ajoute une valeur à la fin
+// void pousse_file(Liste* liste, int valeur)
+void pousse_file(Liste *liste, int valeur)
 {
-
+    ajoute(liste, valeur);
 }
 
-
-void initialise(DynaTableau* tableau, int capacite)
+// Enlève la première valeur ajoutée et la retourne
+// int retire_file(Liste* liste)
+int retire_file(Liste *liste)
 {
+    Noeud *currentNode = liste->premier;
 
+    if (est_vide(liste))
+    {
+        std::cout << "Empty list !" << std::endl;
+
+        return -1;
+    }
+
+    if (liste->premier->suivant == nullptr)
+    {
+        int value = liste->premier->donnee;
+
+        liste->premier = nullptr;
+
+        return value;
+    }
+
+    Noeud *newPremier = liste->premier->suivant;
+
+    delete liste->premier;
+
+    liste->premier = newPremier;
+
+    return currentNode->donnee;
 }
 
-bool est_vide(const DynaTableau* liste)
+// Ajoute une valeur au début de la structure
+// void pousse_pile(Liste* liste, int valeur)
+void pousse_pile(Liste *liste, int valeur)
 {
-    return false;
+    Noeud *newNode = new Noeud();
+
+    if (!newNode)
+    {
+        std::cout << "Allocation failed" << std::endl;
+        return;
+    }
+
+    newNode->donnee = valeur;
+
+    newNode->suivant = liste->premier;
+
+    liste->premier = newNode;
 }
 
-void affiche(const DynaTableau* tableau)
+// Enlève la dernière valeur ajoutée et la retourne
+// int retire_pile(Liste* liste)
+int retire_pile(Liste *liste)
 {
+    Noeud *currentNode = liste->premier;
 
+    while (currentNode->suivant->suivant != nullptr)
+    {
+        currentNode = currentNode->suivant;
+    }
+
+    int value = currentNode->suivant->donnee;
+
+    delete currentNode->suivant;
+
+    currentNode->suivant = nullptr;
+
+    return value;
 }
-
-int recupere(const DynaTableau* tableau, int n)
-{
-    return 0;
-}
-
-int cherche(const DynaTableau* tableau, int valeur)
-{
-    return -1;
-}
-
-void stocke(DynaTableau* tableau, int n, int valeur)
-{
-
-}
-
-//void pousse_file(DynaTableau* liste, int valeur)
-void pousse_file(Liste* liste, int valeur)
-{
-
-}
-
-//int retire_file(Liste* liste)
-int retire_file(Liste* liste)
-{
-    return 0;
-}
-
-//void pousse_pile(DynaTableau* liste, int valeur)
-void pousse_pile(Liste* liste, int valeur)
-{
-
-}
-
-//int retire_pile(DynaTableau* liste)
-int retire_pile(Liste* liste)
-{
-    return 0;
-}
-
 
 int main()
 {
@@ -131,9 +253,10 @@ int main()
         std::cout << "Oups y a une anguille dans mon tableau" << std::endl;
     }
 
-    for (int i=1; i<=7; i++) {
-        ajoute(&liste, i*7);
-        ajoute(&tableau, i*5);
+    for (int i = 1; i <= 7; i++)
+    {
+        ajoute(&liste, i * 7);
+        ajoute(&tableau, i * 5);
     }
 
     if (est_vide(&liste))
@@ -171,13 +294,16 @@ int main()
     initialise(&pile);
     initialise(&file);
 
-    for (int i=1; i<=7; i++) {
+    for (int i = 1; i <= 7; i++)
+    {
         pousse_file(&file, i);
         pousse_pile(&pile, i);
     }
 
+    std::cout << "retire_file :" << std::endl;
+
     int compteur = 10;
-    while(!est_vide(&file) && compteur > 0)
+    while (!est_vide(&file) && compteur > 0)
     {
         std::cout << retire_file(&file) << std::endl;
         compteur--;
@@ -189,7 +315,7 @@ int main()
     }
 
     compteur = 10;
-    while(!est_vide(&pile) && compteur > 0)
+    while (!est_vide(&pile) && compteur > 0)
     {
         std::cout << retire_pile(&pile) << std::endl;
         compteur--;

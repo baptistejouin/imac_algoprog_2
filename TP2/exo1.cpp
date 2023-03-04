8f3ae141acea61664c2679e531e46eb8e8d346a5
baptistejouin <baptiste.jouin44@gmail.com>
2023-03-04 14:40:55 +0100

    exo1

--- TP2/exo1.cpp
+++ TP2/exo1.cpp
@@ -3,28 +3,29 @@
 
 #include "tp2.h"
 
-MainWindow* w = nullptr;
+MainWindow *w = nullptr;
 
-void selectionSort(Array& toSort) {
+void selectionSort(Array &toSort)
+{
     for (size_t i = 0; i < toSort.size(); i++)
     {
-        for (uint j=0; j<toSort.size(); j++)
+        for (uint j = i; j < toSort.size(); j++)
         {
-            if (toSort.get(j) > toSort.get(i))
+            if (toSort.get(j) < toSort.get(i))
                 toSort.swap(i, j);
         }
     }
-    
-	// selectionSort
+
+    // selectionSort
 }
 
 int main(int argc, char *argv[])
 {
-	QApplication a(argc, argv);
-    uint elementCount=15; // number of elements to sort
+    QApplication a(argc, argv);
+    uint elementCount = 15;                 // number of elements to sort
     MainWindow::instruction_duration = 100; // delay between each array access (set, get, insert, ...)
-    w = new TestMainWindow(selectionSort); // window which display the behavior of the sort algorithm
+    w = new TestMainWindow(selectionSort);  // window which display the behavior of the sort algorithm
     w->show();
 
-	return a.exec();
+    return a.exec();
 }

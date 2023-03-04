b14572445a3c5a53ee4863ca39287df10aabc251
baptistejouin <baptiste.jouin44@gmail.com>
2023-03-04 15:00:54 +0100

    exo2

--- TP2/exo2.cpp
+++ TP2/exo2.cpp
@@ -3,22 +3,44 @@
 
 #include "tp2.h"
 
-MainWindow* w=nullptr;
+MainWindow *w = nullptr;
 
-void insertionSort(Array& toSort){
-	Array& sorted=w->newArray(toSort.size());
+void insertionSort(Array &toSort)
+{
+	Array &sorted = w->newArray(toSort.size());
 
 	// insertion sort from toSort to sorted
-	
-	toSort=sorted; // update the original array
+	sorted[0] = toShort[0];
+
+	for (size_t n = 1; n < toShort.size(); n++)
+	{
+		int isFound = false;
+
+		for (m = 0; m < toShort.size(); m++)
+		{
+			if (sorted[m] > toShort[n])
+			{
+				sorted.insert(m, toShort[n]);
+				isFound = true;
+				break;
+			}
+		}
+
+		if (!isFound)
+		{
+			sorted.insert(n, toShort[n])
+		}
+	}
+
+	toSort = sorted; // update the original array
 }
 
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
-    uint elementCount=15; // number of elements to sort
-    MainWindow::instruction_duration = 100; // delay between each array access (set, get, insert, ...)
-    w = new TestMainWindow(insertionSort); // window which display the behavior of the sort algorithm
+	uint elementCount = 15;					// number of elements to sort
+	MainWindow::instruction_duration = 100; // delay between each array access (set, get, insert, ...)
+	w = new TestMainWindow(insertionSort);	// window which display the behavior of the sort algorithm
 	w->show();
 
 	return a.exec();

6a257a76dec1764e26ab83ba1c67b309b6405bd8
baptistejouin <baptiste.jouin44@gmail.com>
2023-03-05 16:26:25 +0100

    exo1

--- TP1/exo1.cpp
+++ TP1/exo1.cpp
@@ -9,16 +9,27 @@ long power(long value, long n)
     Context _("power", value, n); // do not care about this, it allow the display of call stack
 
     // your code
+    if (n == 0)
+    {
+        return_and_display(1);
+    }
+    else if (n == 1)
+    {
+        return_and_display(value);
+    }
+
+    value *= power(value, n - 1);
+
     // return_and_display shows the result in the window and returns it
-    return_and_display(1);
+    return_and_display(value);
 }
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);  // create a window manager
-    MainWindow::instruction_duration = 400;  // make a pause between instruction display
-    MainWindow* w = new PowerWindow(power); // create a window for this exercice
-    w->show(); // show exercice
+    QApplication a(argc, argv);             // create a window manager
+    MainWindow::instruction_duration = 400; // make a pause between instruction display
+    MainWindow *w = new PowerWindow(power); // create a window for this exercice
+    w->show();                              // show exercice
 
     return a.exec(); // main loop while window is opened
 }

e38754c9f0e4bf9781fea9b3a2f5f4379fc50b56
baptistejouin <baptiste.jouin44@gmail.com>
2023-03-05 17:02:21 +0100

    exo3

--- TP1/exo3.cpp
+++ TP1/exo3.cpp
@@ -4,26 +4,30 @@
 
 #define return_and_display(result) return _.store(result);
 
+MainWindow *w = nullptr;
 
-int search(int value, Array& toSort, int size)
+int search(int value, Array &toSort, int size)
 {
     Context _("search", value, size); // do not care about this, it allow the display of call stack
 
     // your code
+
+    if (size <= 0)
+        return_and_display(-1);
+    if (value == toSort[size - 1])
+        return_and_display(size - 1);
+
     // check the last cell and if it does not correspond
     // then consider a smaller array when recalling search
+    return_and_display(search(value, toSort, size - 1));
 }
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);  // create a window manager
-    MainWindow::instruction_duration = 400;  // make a pause between instruction display
-    MainWindow* w = new SearchWindow(search); // create a window for this exercice
-    w->show(); // show exercice
+    QApplication a(argc, argv);               // create a window manager
+    MainWindow::instruction_duration = 400;   // make a pause between instruction display
+    MainWindow *w = new SearchWindow(search); // create a window for this exercice
+    w->show();                                // show exercice
 
     return a.exec(); // main loop while window is opened
 }
-
-
-
-

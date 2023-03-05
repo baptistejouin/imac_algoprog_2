828138123b1085c25ababf3df5ad70948d489ff6
baptistejouin <baptiste.jouin44@gmail.com>
2023-03-05 16:41:14 +0100

    exo2

--- TP1/exo2.cpp
+++ TP1/exo2.cpp
@@ -9,16 +9,18 @@ int fibonacci(int value)
     Context _("fibonacci", value); // do not care about this, it allow the display of call stack
 
     // your code
-
-    return_and_display(value);
+    // F0 = 0, F1 = 1
+    if (value <= 1)
+        return_and_display(value);
+    return_and_display((fibonacci(value - 1) + fibonacci(value - 2)));
 }
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);  // create a window manager
-    MainWindow::instruction_duration = 400;  // make a pause between instruction display
-    MainWindow* w = new FibonacciWindow(fibonacci); // create a window for this exercice
-    w->show(); // show exercice
+    QApplication a(argc, argv);                     // create a window manager
+    MainWindow::instruction_duration = 400;         // make a pause between instruction display
+    MainWindow *w = new FibonacciWindow(fibonacci); // create a window for this exercice
+    w->show();                                      // show exercice
 
     return a.exec(); // main loop while window is opened
 }

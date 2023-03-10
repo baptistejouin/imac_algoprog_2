0d781f2ed8c3547c1b8787e21f7bf3a7e105e6a4
baptistejouin <baptiste.jouin44@gmail.com>
2023-03-10 12:42:42 +0100

    exo2

--- TP3/exo2.cpp
+++ TP3/exo2.cpp
@@ -2,8 +2,65 @@
 #include <QApplication>
 #include <time.h>
 
-MainWindow* w = nullptr;
+MainWindow *w = nullptr;
 using std::size_t;
+
+int binarySearchLeft(Array &array, int toSearch)
+{
+	int start = 0, end = array.size(), findIndex;
+
+	while (start < end)
+	{
+		int mid = (start + end) / 2;
+
+		if (toSearch > array.get(mid))
+		{
+			start = mid + 1;
+		}
+		else if (toSearch < array.get(mid))
+		{
+			end = mid;
+		}
+		else
+		{
+			end = mid;
+			findIndex = mid;
+		}
+	}
+
+	if (array.get(findIndex) == toSearch)
+		return findIndex;
+	else
+		return -1;
+}
+int binarySearchRight(Array &array, int toSearch)
+{
+	int start = 0, end = array.size(), findIndex;
+
+	while (start < end)
+	{
+		int mid = (start + end) / 2;
+
+		if (toSearch > array.get(mid))
+		{
+			start = mid + 1;
+		}
+		else if (toSearch < array.get(mid))
+		{
+			end = mid;
+		}
+		else
+		{
+			start = mid + 1;
+			findIndex = mid;
+		}
+	}
+
+	if (array.get(findIndex) == toSearch)
+		return findIndex;
+	else
+		return -1;
+}
 
 /**
  * @brief define indexMin and indexMax as the first and the last index of toSearch
@@ -12,10 +69,13 @@ using std::size_t;
  * @param indexMin first index of the value to find
  * @param indexMax last index of the value to find
  */
-void binarySearchAll(Array& array, int toSearch, int& indexMin, int& indexMax)
+void binarySearchAll(Array &array, int toSearch, int &indexMin, int &indexMax)
 {
 	// do not use increments, use two different binary search loop
-    indexMin = indexMax = -1;
+	indexMin = indexMax = -1;
+
+	indexMin = binarySearchLeft(array, toSearch);
+	indexMax = binarySearchRight(array, toSearch);
 }
 
 int main(int argc, char *argv[])
